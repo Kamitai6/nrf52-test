471ac7895516a56cbcd70733bf510c3a5a060f6b
Kamitai6 <reenigne.6.8116@gmail.com>
2025-01-20 17:09:16 +0900

    回路が間違っていることが分かったけど、いったんコミット

---
+++ src/MLX90363.rs
@@ -0,0 +1,80 @@
+pub struct MLX90363<SPI, SS> {
+    spi: SPI,
+    ss: SS,
+    spi_read_buffer: [u8; 8],
+    spi_write_buffer: [u8; 8],
+    angle_degrees: f32,
+    angle_lsb: u16,
+    error_lsb: u8,
+    roll_counter: u8,
+    virtual_gain: u8,
+    crc: u8,
+}
+
+impl<SPI, SS, E> MLX90363<SPI, SS>
+where
+    SPI: Transfer<u8, Error = E> + Write<u8, Error = E>,
+    SS: OutputPin,
+{
+    pub fn new(spi: SPI, ss: SS) -> Self {
+        Self {
+            spi,
+            ss,
+            spi_read_buffer: [0; 8],
+            spi_write_buffer: [0; 8],
+            angle_degrees: 0.0,
+            angle_lsb: 0,
+            error_lsb: 0,
+            roll_counter: 0,
+            virtual_gain: 0,
+            crc: 0,
+        }
+    }
+
+    fn angle_lsb(&mut self) -> u16 {
+        self.angle_lsb =
+            ((self.spi_read_buffer[1] & 0x3F) as u16) << 8 | (self.spi_read_buffer[0] as u16);
+        self.angle_lsb
+    }
+
+    fn angle_degrees(&mut self) -> f32 {
+        const LSB_TO_DEGREES: f32 = 0.02197;
+        self.angle_lsb();
+        self.angle_degrees = self.angle_lsb as f32 * LSB_TO_DEGREES;
+        self.angle_degrees
+    }
+
+    fn error_bits(&self) -> u8 {
+        self.spi_read_buffer[1] >> 6
+    }
+
+    fn rolling_counter(&self) -> u8 {
+        self.spi_read_buffer[6] & 0x3F
+    }
+
+    fn virtual_gain(&self) -> u8 {
+        self.spi_read_buffer[4]
+    }
+
+    fn crc(&self) -> u8 {
+        self.spi_read_buffer[7]
+    }
+
+    pub fn read_data(&mut self) -> Result<(), E> {
+        self.spi_write_buffer = [0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0x13, 0xEA];
+
+        self.ss.set_low().ok();
+        self.spi.transfer(&mut self.spi_write_buffer)?;
+        self.spi_read_buffer.copy_from_slice(&self.spi_write_buffer);
+        self.ss.set_high().ok();
+
+        self.angle_lsb = self.angle_lsb();
+        self.angle_degrees = self.angle_degrees();
+        self.error_lsb = self.error_bits();
+        self.roll_counter = self.rolling_counter();
+        self.virtual_gain = self.virtual_gain();
+        self.crc = self.crc();
+
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,198 +1,92 @@
-// #![no_std]
-// #![no_main]
-// mod defaults;
-// // mod sensor;
-
-// use cortex_m_rt::entry;
-// use embassy_executor::Executor;
-// use embassy_stm32::gpio::{Level, Output, Speed};
-// use embassy_stm32::mode::Async;
-// use embassy_stm32::mode::Blocking;
-// use embassy_stm32::{spi, Config};
-// use embassy_time::{Duration, Timer};
-// use heapless::String;
-// use panic_halt as _;
-// use rtt_target::{rprintln, rtt_init_print};
-// use static_cell::StaticCell;
-
-// // #[embassy_executor::task]
-// // async fn main_task(mut ss: Output<'static>, mut spi: spi::Spi<'static, Async>) {
-// //     let mut read_buffer = [0u16]; // ダミーデータ
-// //     let mut write_buffer: [u16; 1] = [0x3FFF]; // 角度読み取りコマンド
-
-// //     loop {
-// //         // SPI経由でデータを送受信
-// //         ss.set_low();
-// //         rprintln!("Sending read command");
-// //         // spi.transfer(&mut read_buffer, &write_buffer).await.ok();
-// //         spi.write(&write_buffer).await.ok();
-// //         spi.read(&mut read_buffer).await.ok();
-// //         // unsafe {
-// //         //     let result = spi.blocking_transfer_in_place(&mut write_buffer);
-// //         //     if let Err(_) = result {
-// //         //         rprintln!("crap");
-// //         //     }
-// //         // }
-// //         rprintln!("Received angle: {}", read_buffer[0]);
-// //         ss.set_high();
-// //         write_buffer[0] = 0x0000;
-// //         Timer::after(Duration::from_millis(1)).await;
-// //         ss.set_low();
-// //         rprintln!("Sending read command");
-// //         spi.write(&write_buffer).await.ok();
-// //         spi.read(&mut read_buffer).await.ok();
-// //         // unsafe {
-// //         //     let result = spi.blocking_transfer_in_place(&mut write_buffer);
-// //         //     if let Err(_) = result {
-// //         //         rprintln!("crap");
-// //         //     }
-// //         // }
-// //         rprintln!("Received angle: {}", read_buffer[0]);
-// //         ss.set_high();
-// //         rprintln!("Finished SPI communication");
-// //     }
-// // }
-
-// // static EXECUTOR: StaticCell<Executor> = StaticCell::new();
-
-// #[entry]
-// fn main() -> ! {
-//     rtt_init_print!();
-//     let mut config = embassy_stm32::Config::default();
-//     {
-//         use embassy_stm32::rcc::*;
-//         config.rcc.ls = LsConfig::off();
-//         config.rcc.hsi = Some(HSIPrescaler::DIV1);
-//         config.rcc.csi = false;
-//         config.rcc.pll1 = Some(Pll {
-//             source: PllSource::HSI,
-//             prediv: PllPreDiv::DIV4, // pllm
-//             mul: PllMul::MUL30,      //plln
-//             divp: Some(PllDiv::DIV2),
-//             divq: Some(PllDiv::DIV2),
-//             divr: Some(PllDiv::DIV2),
-//         });
-//         config.rcc.sys = Sysclk::PLL1_P;
-//         config.rcc.ahb_pre = AHBPrescaler::DIV1;
-//         config.rcc.apb1_pre = APBPrescaler::DIV1;
-//         config.rcc.apb2_pre = APBPrescaler::DIV1;
-//         config.rcc.apb3_pre = APBPrescaler::DIV1;
-//         config.rcc.voltage_scale = VoltageScale::Scale0;
-//     }
-//     let p = embassy_stm32::init(config);
-//     rprintln!("Hello, world!");
-
-//     let mut spi3_ss = Output::new(p.PA15, Level::High, Speed::Low);
-//     let mut spi3_config = embassy_stm32::spi::Config::default();
-//     spi3_config.mode = embassy_stm32::spi::MODE_1;
-//     spi3_config.frequency = embassy_stm32::time::mhz(3);
-//     spi3_config.bit_order = embassy_stm32::spi::BitOrder::MsbFirst;
-//     //     // let mut spi3 = embassy_stm32::spi::Spi::new(
-//     //     //     p.SPI3,
-//     //     //     p.PC10,
-//     //     //     p.PC12,
-//     //     //     p.PC11,
-//     //     //     p.GPDMA2_CH3,
-//     //     //     p.GPDMA2_CH4,
-//     //     //     spi3_config,
-//     //     // );
-//     let mut spi3 =
-//         embassy_stm32::spi::Spi::new_blocking(p.SPI3, p.PC10, p.PC12, p.PC11, spi3_config);
-//     //     // let mut encoder = sensor::encoder::AS5048::new(spi3, spi3_ss);
-
-//     //     // encoder.init();
-//     //     // motor.linkSensor(&encoder);
-//     //     // driver.voltage_power_supply = 12;
-//     //     // driver.pwm_frequency = 15000; // suggested under 18khz
-//     //     // driver.init();
-//     //     // motor.linkDriver(&driver);
-//     //     // current_sense.linkDriver(&driver);
-//     //     // motor.torque_controller = TorqueControlType::voltage;
-//     //     // motor.controller = MotionControlType::torque;
-//     //     // motor.motion_downsample = 0.0;
-//     //     // motor.PID_velocity.P = 0.2;
-//     //     // motor.PID_velocity.I = 5.0;
-//     //     // motor.LPF_velocity.Tf = 0.02;
-//     //     // motor.P_angle.P = 20.0;
-//     //     // motor.LPF_angle.Tf = 0.0;
-//     //     // motor.PID_current_q.P = 3.0;
-//     //     // motor.PID_current_q.I = 100.0;
-//     //     // motor.LPF_current_q.Tf = 0.02;
-//     //     // motor.PID_current_d.P = 3.0;
-//     //     // motor.PID_current_d.I = 100.0;
-//     //     // motor.LPF_current_d.Tf = 0.02;
-//     //     // motor.velocity_limit = 100.0; // 100 rad/s velocity limit
-//     //     // motor.voltage_limit = 12.0; // 12 Volt limit
-//     //     // motor.current_limit = 2.0; // 2 Amp current limit
-//     //     // motor.init();
-//     //     // current_sense.init();
-//     //     // cs.gain_a *= -1;
-//     //     // cs.gain_b *= -1;
-//     //     // cs.gain_c *= -1;
-//     //     // motor.linkCurrentSense(&current_sense);
-//     //     // motor.initFOC();
-
-//     //     // let executor = EXECUTOR.init(Executor::new());
-
-//     //     // executor.run(|spawner| {
-//     //     //     spawner.spawn(main_task(spi3_ss, spi3)).unwrap();
-//     //     // })
-//     let mut read_buffer = [0u16]; // ダミーデータ
-//     let mut write_buffer: [u16; 1] = [0x3FFF]; // 角度読み取りコマンド
-//     loop {
-//         spi3_ss.set_low();
-//         rprintln!("Sending read command");
-//         spi3.blocking_write(&write_buffer).ok();
-//         spi3.blocking_read(&mut read_buffer).ok();
-//         spi3_ss.set_high();
-//         rprintln!("Received angle: {}", read_buffer[0]);
-//     }
-// }
 #![no_std]
 #![no_main]
 
-use cortex_m::{self, delay};
 use cortex_m_rt::entry;
-use embedded_alloc::Heap;
+use nb::block;
 use panic_halt as _;
 use rtt_target::{rprintln, rtt_init_print};
-use stm32h5::stm32h562 as pac;
-
-mod as5048;
-mod stm32;
-
-#[global_allocator]
-static HEAP: Heap = Heap::empty();
+use stm32h7xx_hal::{pac, prelude::*, spi};
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
-    use core::mem::MaybeUninit;
-    const HEAP_SIZE: usize = 1024;
-    static mut HEAP_MEM: [MaybeUninit<u8>; HEAP_SIZE] = [MaybeUninit::uninit(); HEAP_SIZE];
-    unsafe { HEAP.init(HEAP_MEM.as_ptr() as usize, HEAP_SIZE) }
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let dp = pac::Peripherals::take().unwrap();
 
-    let dp = pac::Peripherals::take().unwrap(); //device peripheral
-    let cp = cortex_m::peripheral::Peripherals::take().unwrap(); //cortexm peripheral
-    let mut delay = delay::Delay::new(cp.SYST, 240000000_u32);
+    // Constrain and Freeze power
+    rprintln!("Setup PWR...                  ");
+    let pwr = dp.PWR.constrain();
+    let pwrcfg = pwr.freeze();
 
-    stm32::rcc::clock_init(&dp);
-    let spi = stm32::spi::SPI::new(&dp);
-    let delay_func = move |ms: u32| delay.delay_ms(ms);
-    let mut encoder = as5048::AS5048::new(&spi, delay_func);
-    spi.spi3_init();
+    // Constrain and Freeze clock
+    rprintln!("Setup RCC...                  ");
+    let rcc = dp.RCC.constrain();
+    let ccdr = rcc
+        .sys_ck(100.MHz())
+        .pll1_q_ck(100.MHz())
+        .freeze(pwrcfg, &dp.SYSCFG);
+
+    let gpioa = dp.GPIOA.split(ccdr.peripheral.GPIOA);
+    let gpiob = dp.GPIOB.split(ccdr.peripheral.GPIOB);
+    let gpioc = dp.GPIOC.split(ccdr.peripheral.GPIOC);
+    let gpiod = dp.GPIOD.split(ccdr.peripheral.GPIOD);
+
+    let sck = gpioa.pa12.into_alternate();
+    let miso = gpiob.pb14.into_alternate();
+    let mosi = gpiob.pb15.into_alternate();
+    let mut nss = gpioa.pa11.into_push_pull_output();
+
+    let mut led = gpiod.pd0.into_push_pull_output();
+    let mut delay = cp.SYST.delay(ccdr.clocks);
+
+    let mut spi: spi::Spi<_, _, u8> = dp.SPI2.spi(
+        (sck, miso, mosi),
+        spi::MODE_1,
+        1.MHz(),
+        ccdr.peripheral.SPI2,
+        &ccdr.clocks,
+    );
+    nss.set_high();
+    led.set_high();
 
     loop {
-        // let state = encoder.getDiagnostic();
-        // rprintln!("state: {}", state);
+        delay.delay_us(500_u16);
 
-        // let gain = encoder.getGain();
-        // rprintln!("gain: {}", gain);
+        let mut spi_buffer = [0x00, 0x00, 0xAA, 0xAA, 0x00, 0x00, 0xD0, 0xAB];
 
-        // let error = encoder.getErrors();
-        // rprintln!("error: {}", error);
+        nss.set_low();
+        led.set_low();
+        delay.delay_us(100_u16);
+        let result = spi.transfer(&mut spi_buffer);
+        match result {
+            Ok(values) => {
+                for (i, &value) in values.iter().enumerate() {
+                    rprintln!("Received data {}: {}", i, value);
+                }
+            }
+            Err(e) => rprintln!("Error: {:?}", e),
+        }
+        nss.set_high();
+        led.set_high();
 
-        let rotation = encoder.getRawRotation();
-        rprintln!("rotation: {}", rotation);
+        delay.delay_us(500_u16);
+
+        spi_buffer = [0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0x13, 0xEA];
+        nss.set_low();
+        led.set_low();
+        delay.delay_us(100_u16);
+        let result2 = spi.transfer(&mut spi_buffer);
+        match result2 {
+            Ok(values) => {
+                for (i, &value) in values.iter().enumerate() {
+                    rprintln!("Received data {}: {}", i, value);
+                }
+            }
+            Err(e) => rprintln!("Error: {:?}", e),
+        }
+        // let angle_lsb = ((result2[1] & 0x3F) as u16) << 8 | (result2[0] as u16);
+        // rprintln!("angle {}", angle_lsb);
+        nss.set_high();
+        led.set_high();
     }
 }
