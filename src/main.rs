449895bb4099fbb34fda5b88774472ac7ba833e9
Kamitai6 <reenigne.6.8116@gmail.com>
2025-01-23 21:48:07 +0900

    なんか、readできないけど、writeだけならEthernet動いたぞ

--- src/main.rs
+++ src/main.rs
@@ -1,95 +1,259 @@
 #![no_std]
 #![no_main]
 
-use cortex_m_rt::entry;
-use nb::block;
-use panic_halt as _;
-use rtt_target::{rprintln, rtt_init_print};
-use stm32h7xx_hal::{pac, prelude::*, spi};
+use core::mem::MaybeUninit;
+use core::ptr::addr_of_mut;
+use core::sync::atomic::{AtomicU32, Ordering};
 
-pub struct Drv8343Reg {
-    pub fault_status: u8,
-    pub diag_status: [u8; 3],
-    pub control: [u8; 14],
+use panic_rtt_target as _;
+use rtt_target::{rprintln, rtt_init_print};
+
+use rtic::app;
+use rtic_monotonics::systick::prelude::*;
+
+use smoltcp::iface::{Config, Interface, SocketSet, SocketStorage};
+use smoltcp::time::Instant;
+use smoltcp::wire::{HardwareAddress, IpAddress, IpCidr};
+
+use stm32h7xx_hal::{ethernet, rcc::CoreClocks, stm32};
+mod DP83848;
+
+/// Configure SYSTICK for 1ms timebase
+// fn systick_init(syst: &mut stm32::SYST, clocks: CoreClocks) {
+//     let c_ck_mhz = clocks.c_ck().to_MHz();
+
+//     let syst_calib = 0x3E8;
+
+//     syst.set_clock_source(cortex_m::peripheral::syst::SystClkSource::Core);
+//     syst.set_reload((syst_calib * c_ck_mhz) - 1);
+//     syst.enable_interrupt();
+//     syst.enable_counter();
+// }
+// systick_monotonic!(Mono, 1000);
+
+// ======================================================================
+// Entry point
+// ======================================================================
+
+/// TIME is an atomic u32 that counts milliseconds. Although not used
+/// here, it is very useful to have for network protocols
+static TIME: AtomicU32 = AtomicU32::new(0);
+
+/// Locally administered MAC address
+const MAC_ADDRESS: [u8; 6] = [0x02, 0x00, 0x11, 0x22, 0x33, 0x44];
+
+/// Ethernet descriptor rings are a global singleton
+#[link_section = ".sram3.eth"]
+static mut DES_RING: MaybeUninit<ethernet::DesRing<4, 4>> =
+    MaybeUninit::uninit();
+
+/// Net storage with static initialisation - another global singleton
+pub struct NetStorageStatic<'a> {
+    socket_storage: [SocketStorage<'a>; 8],
 }
 
-#[entry]
-fn main() -> ! {
-    rtt_init_print!();
-    let cp = cortex_m::Peripherals::take().unwrap();
-    let dp = pac::Peripherals::take().unwrap();
+// MaybeUninit allows us write code that is correct even if STORE is not
+// initialised by the runtime
+static mut STORE: MaybeUninit<NetStorageStatic> = MaybeUninit::uninit();
 
-    // Constrain and Freeze power
-    rprintln!("Setup PWR...                  ");
-    let pwr = dp.PWR.constrain();
-    let pwrcfg = pwr.freeze();
+pub struct Net<'a> {
+    iface: Interface,
+    ethdev: ethernet::EthernetDMA<4, 4>,
+    sockets: SocketSet<'a>,
+}
+impl<'a> Net<'a> {
+    pub fn new(
+        store: &'a mut NetStorageStatic<'a>,
+        mut ethdev: ethernet::EthernetDMA<4, 4>,
+        ethernet_addr: HardwareAddress,
+        now: Instant,
+    ) -> Self {
+        let config = Config::new(ethernet_addr);
+        let mut iface = Interface::new(config, &mut ethdev, now);
+        // Set IP address
+        iface.update_ip_addrs(|addrs| {
+            let _ = addrs.push(IpCidr::new(IpAddress::v4(192, 168, 1, 99), 0));
+        });
 
-    // Constrain and Freeze clock
-    rprintln!("Setup RCC...                  ");
-    let rcc = dp.RCC.constrain();
-    let ccdr = rcc
-        .sys_ck(100.MHz())
-        .pll1_q_ck(100.MHz())
-        .freeze(pwrcfg, &dp.SYSCFG);
+        let sockets = SocketSet::new(&mut store.socket_storage[..]);
 
-    let gpioa = dp.GPIOA.split(ccdr.peripheral.GPIOA);
-    let gpiob = dp.GPIOB.split(ccdr.peripheral.GPIOB);
-    let gpioc = dp.GPIOC.split(ccdr.peripheral.GPIOC);
-    let gpiod = dp.GPIOD.split(ccdr.peripheral.GPIOD);
-
-    let sck = gpioc.pc10.into_alternate();
-    let miso = gpioc.pc11.into_alternate();
-    let mosi = gpioc.pc12.into_alternate();
-    let mut nss = gpioa.pa15.into_push_pull_output();
-
-    let mut led = gpiod.pd0.into_push_pull_output();
-    let mut delay = cp.SYST.delay(ccdr.clocks);
-
-    let mut spi: spi::Spi<_, _, u16> = dp.SPI3.spi(
-        (sck, miso, mosi),
-        spi::MODE_1, // or MODE_1?
-        500.kHz(),
-        ccdr.peripheral.SPI3,
-        &ccdr.clocks,
-    );
-    nss.set_high();
-    led.set_high();
-
-    delay.delay_us(100_u16);
-
-    let mut spi_buffer: [u16; 1] = [(0b1 << 15) | (0b0000000 << 8) | 0b00000000]; //fault statusを読みたい
-
-    nss.set_low();
-    led.set_low();
-    let result = spi.transfer(&mut spi_buffer);
-    match result {
-        Ok(values) => {
-            for (i, &value) in values.iter().enumerate() {
-                rprintln!("Received data {}: {}", i, value);
-            }
+        Net::<'a> {
+            iface,
+            ethdev,
+            sockets,
         }
-        Err(e) => rprintln!("Error: {:?}", e),
     }
-    nss.set_high();
-    led.set_high();
 
-    loop {
-        
-        delay.delay_us(800_u16);
+    /// Polls on the ethernet interface. You should refer to the smoltcp
+    /// documentation for poll() to understand how to call poll efficiently
+    pub fn poll(&mut self, now: i64) {
+        let timestamp = Instant::from_millis(now);
 
-        spi_buffer = [(0b1 << 15) | (0b0000000 << 8) | 0b00000000];
-        nss.set_low();
-        led.set_low();
-        let result2 = spi.transfer(&mut spi_buffer);
-        match result2 {
-            Ok(values) => {
-                for (i, &value) in values.iter().enumerate() {
-                    rprintln!("Received data {}: {}", i, value);
-                }
-            }
-            Err(e) => rprintln!("Error: {:?}", e),
+        self.iface
+            .poll(timestamp, &mut self.ethdev, &mut self.sockets);
+    }
+}
+
+#[app(device = stm32h7xx_hal::stm32, peripherals = true)]
+mod app {
+    use stm32h7xx_hal::{ethernet, ethernet::PHY, gpio, prelude::*};
+    use DP83848::DP83848;
+
+    use super::*;
+    use core::sync::atomic::Ordering;
+
+    #[shared]
+    struct SharedResources {}
+    #[local]
+    struct LocalResources {
+        net: Net<'static>,
+        dp83848: DP83848<ethernet::EthernetMAC>,
+        link_led: gpio::gpiod::PD0<gpio::Output<gpio::PushPull>>,
+    }
+
+    #[init]
+    fn init(mut ctx: init::Context) -> (SharedResources, LocalResources) {
+        rtt_init_print!();
+        // Initialise power...
+        rprintln!("Setup PWR...                  ");
+        let pwr = ctx.device.PWR.constrain();
+        let pwrcfg = pwr.freeze();
+
+        // Link the SRAM3 power state to CPU1
+        ctx.device.RCC.ahb2enr.modify(|_, w| w.sram3en().set_bit());
+
+        // Initialise clocks...
+        rprintln!("Setup RCC...                  ");
+        let rcc = ctx.device.RCC.constrain();
+        let ccdr = rcc
+            .sys_ck(200.MHz())
+            .hclk(200.MHz())
+            .freeze(pwrcfg, &ctx.device.SYSCFG);
+
+        // Initialise system...
+        ctx.core.SCB.enable_icache();
+        // TODO: ETH DMA coherence issues
+        // ctx.core.SCB.enable_dcache(&mut ctx.core.CPUID);
+        ctx.core.DWT.enable_cycle_counter();
+
+        // Initialise IO...
+        let gpioa = ctx.device.GPIOA.split(ccdr.peripheral.GPIOA);
+        let gpiob = ctx.device.GPIOB.split(ccdr.peripheral.GPIOB);
+        let gpioc = ctx.device.GPIOC.split(ccdr.peripheral.GPIOC);
+        let gpiod = ctx.device.GPIOD.split(ccdr.peripheral.GPIOD);
+        let mut link_led = gpiod.pd0.into_push_pull_output();
+        link_led.set_high();
+
+        let rmii_ref_clk = gpioa.pa1.into_alternate();
+        let rmii_mdio = gpioa.pa2.into_alternate();
+        let rmii_mdc = gpioc.pc1.into_alternate();
+        let rmii_crs_dv = gpioa.pa7.into_alternate();
+        let rmii_rxd0 = gpioc.pc4.into_alternate();
+        let rmii_rxd1 = gpioc.pc5.into_alternate();
+        let rmii_tx_en = gpiob.pb11.into_alternate();
+        let rmii_txd0 = gpiob.pb12.into_alternate();
+        let rmii_txd1 = gpiob.pb13.into_alternate();
+
+        // Initialise ethernet...
+        assert_eq!(ccdr.clocks.hclk().raw(), 200_000_000); // HCLK 200MHz
+        assert_eq!(ccdr.clocks.pclk1().raw(), 100_000_000); // PCLK 100MHz
+        assert_eq!(ccdr.clocks.pclk2().raw(), 100_000_000); // PCLK 100MHz
+        assert_eq!(ccdr.clocks.pclk4().raw(), 100_000_000); // PCLK 100MHz
+
+        let mac_addr = smoltcp::wire::EthernetAddress::from_bytes(&MAC_ADDRESS);
+        let (eth_dma, eth_mac) = unsafe {
+            #[allow(static_mut_refs)] // TODO: Fix this
+            DES_RING.write(ethernet::DesRing::new());
+
+            ethernet::new(
+                ctx.device.ETHERNET_MAC,
+                ctx.device.ETHERNET_MTL,
+                ctx.device.ETHERNET_DMA,
+                (
+                    rmii_ref_clk,
+                    rmii_mdio,
+                    rmii_mdc,
+                    rmii_crs_dv,
+                    rmii_rxd0,
+                    rmii_rxd1,
+                    rmii_tx_en,
+                    rmii_txd0,
+                    rmii_txd1,
+                ),
+                #[allow(static_mut_refs)] // TODO: Fix this
+                DES_RING.assume_init_mut(),
+                mac_addr,
+                ccdr.peripheral.ETH1MAC,
+                &ccdr.clocks,
+            )
+        };
+
+        // Initialise ethernet PHY...
+        let mut dp83848 = DP83848::new(eth_mac);
+        dp83848.phy_reset();
+        for i in 0..5000000 {
+
         }
-        nss.set_high();
-        led.set_high();
+        dp83848.phy_init();
+        // The eth_dma should not be used until the PHY reports the link is up
+
+        unsafe { ethernet::enable_interrupt() };
+
+        // unsafe: mutable reference to static storage, we only do this once
+        let store = unsafe {
+            #[allow(static_mut_refs)] // TODO: Fix this
+            let store_ptr = STORE.as_mut_ptr();
+
+            // Initialise the socket_storage field. Using `write` instead of
+            // assignment via `=` to not call `drop` on the old, uninitialised
+            // value
+            addr_of_mut!((*store_ptr).socket_storage)
+                .write([SocketStorage::EMPTY; 8]);
+
+            // Now that all fields are initialised we can safely use
+            // assume_init_mut to return a mutable reference to STORE
+            #[allow(static_mut_refs)] // TODO: Fix this
+            STORE.assume_init_mut()
+        };
+
+        let net = Net::new(store, eth_dma, mac_addr.into(), Instant::ZERO);
+
+        // 1ms tick
+        // systick_init(ctx.core.SYST, ccdr.clocks);
+
+        (
+            SharedResources {},
+            LocalResources {
+                net,
+                dp83848,
+                link_led,
+            },
+        )
+    }
+
+    // #[idle(local = [dp83848])]
+    #[idle(local = [dp83848, link_led])]
+    fn idle(ctx: idle::Context) -> ! {
+        loop {
+            // Ethernet
+            let status = ctx.local.dp83848.poll_link();
+            rprintln!("status: {}", status);
+            if status == 0 {
+                ctx.local.link_led.set_low();
+            }
+        }
+    }
+
+    #[task(binds = ETH, local = [net])]
+    fn ethernet_event(ctx: ethernet_event::Context) {
+        unsafe { ethernet::interrupt_handler() }
+
+        let time = TIME.load(Ordering::Relaxed);
+        ctx.local.net.poll(time as i64);
+    }
+
+    #[task(binds = SysTick, priority=15)]
+    fn systick_tick(_: systick_tick::Context) {
+        TIME.fetch_add(1, Ordering::Relaxed);
     }
 }
