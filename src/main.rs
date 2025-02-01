d43d5ab5cb24ec694fb33faa086353399315189e
Kamitai6 <reenigne.6.8116@gmail.com>
2025-02-01 11:58:09 +0900

    spiのdmaのtransfer errorが解決できない

--- src/main.rs
+++ src/main.rs
@@ -6,139 +6,199 @@ use rtt_target::{rprintln, rtt_init_print};
 
 use core::cell::{Cell, RefCell};
 
-use cortex_m::peripheral::NVIC;
-use cortex_m::delay::Delay;
-use cortex_m_rt::entry;
+use rtic::app;
+
+// use cmsis_dsp_api as dsp_api;
+// use cmsis_dsp_sys as dsp_sys;
+// use cortex_m::peripheral::NVIC;
+// use cortex_m::delay::Delay;
+// use cortex_m::interrupt::free;
+// use cortex_m_rt::entry;
 use hal::{
     clocks::{Clocks, PllCfg},
     dma::{self, Dma, DmaChannel, DmaInput, DmaInterrupt, DmaPeriph},
     gpio::{self, Pin, PinMode, Port},
     low_power,
-    pac::{self, interrupt},
+    pac::{self, DMA1, SPI2, interrupt},
     prelude::*,
     spi::{self, BaudRate, Spi, SpiConfig, SpiMode},
 };
 
-// Byte 0 is for the address we pass in the `write` transfer; relevant data is in the rest of
-// the values.
 static mut SPI_READ_BUF: [u8; 8] = [0; 8];
 static mut SPI_WRITE_BUF: [u8; 8] = [0x00, 0x00, 0xAA, 0xAA, 0x00, 0x00, 0xD0, 0xAB];
 
-#[entry]
-fn main() -> ! {
-    rtt_init_print!();
-    rprintln!("Start!!!");
-    // Set up CPU peripherals
-    let mut cp = cortex_m::Peripherals::take().unwrap();
-    // Set up microcontroller peripherals
-    let mut dp = pac::Peripherals::take().unwrap();
+#[app(device = pac, peripherals = false)]
+mod app {
+    use super::*;
 
-    let mut clock_cfg = Clocks::default(); //400MHz
-
-    // ---------------clock---------------
-    // hsi(pllsrc) / divm -> pll1-ref-clk
-    // pll1-ref-clk * divp(or divq or divr) -> pll1p(or pll1q or pll1r) clk
-    clock_cfg.pll1 = PllCfg {
-        enabled: true,
-        // fractional: false,
-        pllp_en: true,
-        pllq_en: true,
-        pllr_en: true,
-        divm: 32,// 400MHz / 32 = 12.5MHz
-        divn: 400,
-        divp: 2,// pll1p clock = 12.5 * 2 = 25MHz
-        divq: 8,// pll1q clock = 12.5 * 8 = 100MHz
-        divr: 2,// pll1r clock = 12.5 * 2 = 25MHz
-    };
-
-    clock_cfg.setup().unwrap();
-
-    // Configure pins for Spi
-    let _sck = Pin::new(Port::A, 12, PinMode::Alt(12));
-    let _miso = Pin::new(Port::B, 14, PinMode::Alt(14));
-    let _mosi = Pin::new(Port::B, 15, PinMode::Alt(15));
-
-    let mut cs = Pin::new(Port::A, 11, PinMode::Output);
-
-    let mut led = Pin::new(Port::D, 0, PinMode::Output);
-    let mut delay = Delay::new(cp.SYST, clock_cfg.systick());
-
-    let spi_cfg = SpiConfig {
-        mode: SpiMode::mode1(),
-        // `SpiConfig::default` is mode 0, full duplex, with software CS.
-        ..Default::default()
-    };
-
-    // Set up an SPI peripheral, running at 4Mhz, in SPI mode 0.
-    let mut spi = Spi::new(
-        dp.SPI2,
-        spi_cfg,
-        BaudRate::Div64, // 100MHz / 64 = 1.5625MHz
-    );
-
-    // Set up DMA, for nonblocking (generally faster) conversion transfers:
-    // let mut dma = Dma::new(dp.DMA1);
-
-    // // Associate a pair of DMA channels with SPI1: One for transmit; one for receive.
-    // // Note that mux is not used on F3, F4, and most L4s: DMA channels are hard-coded
-    // // to peripherals on those platforms.
-    // dma::mux(DmaPeriph::Dma2, DmaChannel::C1, DmaInput::Spi2Tx);
-    // dma::mux(DmaPeriph::Dma2, DmaChannel::C2, DmaInput::Spi2Rx);
-
-    // unsafe {
-    //     // Write to SPI, using DMA.
-    //     // spi.write_dma(&write_buf, DmaChannel::C1, Default::default(), DmaPeriph::Dma2);
-
-    //     // Read (transfer) from SPI, using DMA.
-    //     spi.transfer_dma(
-    //         // Write buffer, starting with the registers we'd like to access, and 0-padded to
-    //         // read 3 bytes.
-    //         &SPI_WRITE_BUF,
-    //         &mut SPI_READ_BUF,  // Read buf, where the data will go
-    //         DmaChannel::C1,     // Write channel
-    //         DmaChannel::C2,     // Read channel
-    //         Default::default(), // Write channel config
-    //         Default::default(), // Read channel config
-    //         DmaPeriph::Dma2,
-    //     );
-    // }
-
-    // Alternatively, use the blocking, non-DMA SPI API` (Also supports `embedded-hal` traits):
-
-    // Assign peripheral structs as global, so we can access them in interrupts.
-    // with(|cs| {
-    //     DMA.borrow(cs).replace(Some(dma));
-    //     SPI.borrow(cs).replace(Some(spi));
-    // });
-
-    // Unmask the interrupt line for DMA read complete. See the `DMA_CH3` interrupt handlers below,
-    // where we set CS high, terminal the DMA read, and display the data read.
-    // unsafe {
-    //     NVIC::unmask(pac::Interrupt::DMA1_CH2);
-    // }
-
-    // Alternatively, we can take readings without DMA. This provides a simpler, memory-safe API,
-    // and is compatible with the `embedded_hal::blocking::i2c traits.
-
-    led.set_high();
-
-    let mut spi_buf: [u8; 8] = [0x00, 0x00, 0xAA, 0xAA, 0x00, 0x00, 0xD0, 0xAB];
-    cs.set_low();
-    delay.delay_us(1_00);
-    spi.transfer(&mut spi_buf).ok();
-    let values = spi_buf;
-    for (i, &value) in values.iter().enumerate() {
-        rprintln!("Received data 1 {}: {}", i, value);
+    #[shared]
+    struct Shared {
+        dma: Dma<DMA1>,
+        spi2: Spi<SPI2>,
+        nss: Pin,
     }
-    cs.set_high();
 
-    loop {
-        delay.delay_us(8_00);
-        spi_buf = [0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0x13, 0xEA];
-        cs.set_low();
-        delay.delay_us(1_00);
-        spi.transfer(&mut spi_buf).ok();
-        let values = spi_buf;
+    #[local]
+    struct Local {}
+
+    #[init]
+    fn init(mut ctx: init::Context) -> (Shared, Local) {
+        rtt_init_print!();
+        rprintln!("Start!!!");
+        // Set up CPU peripherals
+        let mut cp = ctx.core;
+        // Set up microcontroller peripherals
+        let mut dp = pac::Peripherals::take().unwrap();
+
+        let mut clock_cfg = Clocks::default(); //400MHz
+
+        // ---------------clock---------------
+        // hsi(pllsrc) / divm -> pll1-ref-clk
+        // pll1-ref-clk * divp(or divq or divr) -> pll1p(or pll1q or pll1r) clk
+        clock_cfg.pll1 = PllCfg {
+            enabled: true,
+            // fractional: false,
+            pllp_en: true,
+            pllq_en: true,
+            pllr_en: true,
+            divm: 32,// 400MHz / 32 = 12.5MHz
+            divn: 400,
+            divp: 2,// pll1p clock = 12.5 * 2 = 25MHz
+            divq: 8,// pll1q clock = 12.5 * 8 = 100MHz
+            divr: 2,// pll1r clock = 12.5 * 2 = 25MHz
+        };
+
+        clock_cfg.setup().unwrap();
+
+        // Configure pins for Spi
+        let _sck = Pin::new(Port::A, 12, PinMode::Alt(12));
+        let _miso = Pin::new(Port::B, 14, PinMode::Alt(14));
+        let _mosi = Pin::new(Port::B, 15, PinMode::Alt(15));
+
+        let mut nss = Pin::new(Port::A, 11, PinMode::Output);
+        nss.set_high();
+        let mut led = Pin::new(Port::D, 0, PinMode::Output);
+        led.set_high();
+
+        // `SpiConfig::default` is mode 0, full duplex, with software CS.
+        let spi2_cfg = SpiConfig {
+            mode: SpiMode::mode1(),
+            ..Default::default()
+        };
+
+        // Set up an SPI peripheral, running at 4Mhz, in SPI mode 0.
+        let mut spi2 = Spi::new(
+            dp.SPI2,
+            spi2_cfg,
+            BaudRate::Div64, // 100MHz / 64 = 1.5625MHz
+        );
+
+        let mut dma = Dma::new(dp.DMA1);
+
+        dma::mux(DmaPeriph::Dma1, DmaChannel::C1, DmaInput::Spi2Tx);
+        dma::mux(DmaPeriph::Dma1, DmaChannel::C2, DmaInput::Spi2Rx);
+
+        dma.enable_interrupt(DmaChannel::C1, DmaInterrupt::TransferComplete);
+        dma.enable_interrupt(DmaChannel::C1, DmaInterrupt::TransferError);
+        dma.enable_interrupt(DmaChannel::C2, DmaInterrupt::TransferComplete);
+
+        (
+            Shared {
+                dma,
+                spi2,
+                nss,
+            },
+            Local {},
+        )
+    }
+
+    #[idle(shared = [spi2, nss])]
+    fn idle(mut ctx: idle::Context) -> ! {
+        (ctx.shared.nss, ctx.shared.spi2).lock(|nss, spi2| {
+            unsafe {
+                SPI_WRITE_BUF = [0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0x13, 0xEA];
+                // SPI_WRITE_BUF = [0x00, 0x00, 0xAA, 0xAA, 0x00, 0x00, 0xD0, 0xAB];
+            }
+    
+            nss.set_low();
+    
+            unsafe {
+                spi2.transfer_dma(
+                    &SPI_WRITE_BUF,
+                    &mut SPI_READ_BUF,
+                    DmaChannel::C1,
+                    DmaChannel::C2,
+                    dma::ChannelCfg {
+                        priority: dma::Priority::Medium,
+                        circular: dma::Circular::Disabled,
+                        // Increment the buffer address, not the peripheral address.
+                        periph_incr: dma::IncrMode::Disabled,
+                        mem_incr: dma::IncrMode::Enabled,
+                    },
+                    Default::default(),
+                    DmaPeriph::Dma1,
+                );
+            }
+            rprintln!("transferd!!!");
+        });
+
+        // let mut nss_is_high = false;
+        // ctx.shared.nss.lock(|nss| {
+        //     nss_is_high = nss.is_high();
+        // });
+        // if nss_is_high {
+        //     (ctx.shared.nss, ctx.shared.spi2).lock(|nss, spi2| {
+        //         unsafe {
+        //             SPI_WRITE_BUF = [0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0x13, 0xEA];
+        //         }
+        
+        //         nss.set_low();
+        
+        //         unsafe {
+        //             spi2.transfer_dma(
+        //                 &SPI_WRITE_BUF,
+        //                 &mut SPI_READ_BUF,
+        //                 DmaChannel::C1,
+        //                 DmaChannel::C2,
+        //                 Default::default(),
+        //                 Default::default(),
+        //                 DmaPeriph::Dma1,
+        //             );
+        //         }
+        //     });
+        // }
+
+        loop {
+        }
+    }
+
+    #[task(binds = DMA1_STR1, shared = [dma], priority = 2)]
+    fn test(mut ctx: test::Context) {
+        (ctx.shared.dma).lock(|dma| {
+            let is = dma.transfer_is_complete(DmaChannel::C1);
+            rprintln!("transfer is complete? : {}", is);
+        });
+        rprintln!("transmit complete");
+    }
+
+    #[task(binds = DMA1_STR2, shared = [spi2, nss], priority = 2)]
+    fn imu_tc_isr(mut ctx: imu_tc_isr::Context) {
+        dma::clear_interrupt(
+            DmaPeriph::Dma1,
+            DmaChannel::C1,
+            DmaInterrupt::TransferComplete,
+        );
+
+        (ctx.shared.spi2).lock(|spi| {
+            // Note that this step is mandatory, per STM32 RM.
+            spi.stop_dma(DmaChannel::C1, Some(DmaChannel::C2), DmaPeriph::Dma1);
+        });
+
+        ctx.shared.nss.lock(|nss| {
+            nss.set_high();
+        });
+
+        let values = unsafe { SPI_READ_BUF };
         for (i, &value) in values.iter().enumerate() {
             rprintln!("Received data 2 {}: {}", i, value);
         }
@@ -152,32 +212,5 @@ fn main() -> ! {
         rprintln!("vgain {}", vgain_lsb);
         let rollcnt_lsb = (values[6] as u16) & 0x3F;
         rprintln!("rollcnt {}", rollcnt_lsb);
-        cs.set_high();
     }
 }
-
-// #[interrupt]
-// /// This interrupt fires when a DMA read is complete
-// fn DMA1_CH2() {
-//     dma::clear_interrupt(
-//         DmaPeriph::Dma2,
-//         DmaChannel::C2,
-//         DmaInterrupt::TransferComplete,
-//     );
-//     with(|cs| {
-//         rprintln!("SPI DMA read complete");
-//         access_global!(SPI, spi, cs);
-//         spi.stop_dma(DmaChannel::C1, Some(DmaChannel::C2), DmaPeriph::Dma2);
-
-//         // See also this convenience function, which clears the interrupt and stops othe Txfer.:
-//         spi.cleanup_dma(DmaPeriph::Dma2, DmaChannel::C1, Some(DmaChannel::C2));
-
-//         unsafe {
-//             // Ignore byte 0, which is the reg we passed during the write.
-//             rprintln!("Data read: {:?}", SPI_READ_BUF);
-//         }
-
-//         // Set CS high.
-//         gpio::set_high(Port::A, 1);
-//     })
-// }
