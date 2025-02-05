9be9a49074ce891b3fe99574f93be9686bc728bc
Kamitai6 <reenigne.6.8116@gmail.com>
2025-02-05 13:04:17 +0900

    とりあえず、SPIのDMA送信も受信も割り込みできた

--- src/main.rs
+++ src/main.rs
@@ -4,123 +4,171 @@
 use panic_rtt_target as _;
 use rtt_target::{rprintln, rtt_init_print};
 
-use core::cell::{Cell, RefCell};
+use rtic::app;
 
-use cortex_m::peripheral::NVIC;
-use cortex_m::delay::Delay;
-use cortex_m_rt::entry;
+// use cmsis_dsp_api as dsp_api;
+// use cmsis_dsp_sys as dsp_sys;
 use hal::{
     clocks::{Clocks, PllCfg},
     dma::{self, Dma, DmaChannel, DmaInput, DmaInterrupt, DmaPeriph},
     gpio::{self, Pin, PinMode, Port},
-    low_power,
-    pac::{self, interrupt},
-    prelude::*,
+    pac::{self, DMA1, SPI2, interrupt},
     spi::{self, BaudRate, Spi, SpiConfig, SpiMode},
 };
 
-// Byte 0 is for the address we pass in the `write` transfer; relevant data is in the rest of
-// the values.
+
+#[link_section = ".sram3"]
 static mut SPI_READ_BUF: [u8; 8] = [0; 8];
-static mut SPI_WRITE_BUF: [u8; 8] = [0x00, 0x00, 0xAA, 0xAA, 0x00, 0x00, 0xD0, 0xAB];
 
-#[entry]
-fn main() -> ! {
-    rtt_init_print!();
-    rprintln!("Start!!!");
-    // Set up CPU peripherals
-    let mut cp = cortex_m::Peripherals::take().unwrap();
-    // Set up microcontroller peripherals
-    let mut dp = pac::Peripherals::take().unwrap();
+#[link_section = ".sram3"]
+static mut SPI_WRITE_BUF: [u8; 8] = [0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0x13, 0xEA];
 
-    let mut clock_cfg = Clocks::default(); //400MHz
+#[app(device = pac, peripherals = false)]
+mod app {
+    use super::*;
 
-    // ---------------clock---------------
-    // hsi(pllsrc) / divm -> pll1-ref-clk
-    // pll1-ref-clk * divp(or divq or divr) -> pll1p(or pll1q or pll1r) clk
-    clock_cfg.pll1 = PllCfg {
-        enabled: true,
-        // fractional: false,
-        pllp_en: true,
-        pllq_en: true,
-        pllr_en: true,
-        divm: 32,
-        divn: 200,
-        divp: 2,// pll1p clock = 12.5 * 2 = 25MHz
-        divq: 4,// pll1q clock = 12.5 * 8 = 100MHz
-        divr: 2,// pll1r clock = 12.5 * 2 = 25MHz
-    };
-
-    clock_cfg.setup().unwrap();
-
-    // Configure pins for Spi
-    let _sck = Pin::new(Port::A, 12, PinMode::Alt(5));
-    let _miso = Pin::new(Port::B, 14, PinMode::Alt(5));
-    let _mosi = Pin::new(Port::B, 15, PinMode::Alt(5));
-
-    let mut cs = Pin::new(Port::A, 11, PinMode::Output);
-
-    let mut led = Pin::new(Port::D, 0, PinMode::Output);
-    let mut delay = Delay::new(cp.SYST, clock_cfg.systick());
-
-    let spi_cfg = SpiConfig {
-        mode: SpiMode::mode1(),
-        // `SpiConfig::default` is mode 0, full duplex, with software CS.
-        ..Default::default()
-    };
-
-    // Set up an SPI peripheral, running at 4Mhz, in SPI mode 0.
-    let mut spi = Spi::new(
-        dp.SPI2,
-        spi_cfg,
-        BaudRate::Div64, // 100MHz / 64 = 1.5625MHz
-    );
-
-    let rcc = unsafe { &*pac::RCC::ptr() };
-    rprintln!("ahb1enr: {:#010x}", rcc.ahb1enr.read().bits());
-    rprintln!("ahb1lpenr: {:#010x}", rcc.ahb1lpenr.read().bits());
-    rprintln!("ahb2enr: {:#010x}", rcc.ahb2enr.read().bits());
-    rprintln!("ahb2lpenr: {:#010x}", rcc.ahb2lpenr.read().bits());
-    rprintln!("ahb3enr: {:#010x}", rcc.ahb3enr.read().bits());
-    rprintln!("ahb3lpenr: {:#010x}", rcc.ahb3lpenr.read().bits());
-    rprintln!("ahb4enr: {:#010x}", rcc.ahb4enr.read().bits());
-    rprintln!("ahb4lpenr: {:#010x}", rcc.ahb4lpenr.read().bits());
-
-    rprintln!("ahb1enr: {:#010x}", rcc.ahb1enr.read().bits());
-    rprintln!("ahb1enr: {:#010x}", rcc.ahb1enr.read().bits());
-    rprintln!("apb1henr: {:#010x}", rcc.apb1henr.read().bits());
-    rprintln!("apb1lenr: {:#010x}", rcc.apb1lenr.read().bits());
-    rprintln!("apb1hlpenr: {:#010x}", rcc.apb1hlpenr.read().bits());
-    rprintln!("apb1llpenr: {:#010x}", rcc.apb1llpenr.read().bits());
-    rprintln!("apb2lpenr: {:#010x}", rcc.apb2lpenr.read().bits());
-    rprintln!("apb2enr: {:#010x}", rcc.apb2enr.read().bits());
-    rprintln!("apb3enr: {:#010x}", rcc.apb3enr.read().bits());
-    rprintln!("apb3lpenr: {:#010x}", rcc.apb3lpenr.read().bits());
-    rprintln!("apb4enr: {:#010x}", rcc.apb4enr.read().bits());
-    rprintln!("apb4lpenr: {:#010x}", rcc.apb4lpenr.read().bits());
-
-    cs.set_high();
-    led.set_high();
-
-    let mut spi_buf: [u8; 8] = [0x00, 0x00, 0xAA, 0xAA, 0x00, 0x00, 0xD0, 0xAB];
-    cs.set_low();
-    // delay.delay_us(1_00);
-    spi.transfer(&mut spi_buf).ok();
-    let values = spi_buf;
-    for (i, &value) in values.iter().enumerate() {
-        rprintln!("Received data 1 {}: {}", i, value);
+    #[shared]
+    struct Shared {
+        dma: Dma<DMA1>,
+        spi2: Spi<SPI2>,
+        nss: Pin,
     }
-    cs.set_high();
 
-    loop {
-        delay.delay_us(8_00);
-        spi_buf = [0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0x13, 0xEA];
-        cs.set_low();
+    #[local]
+    struct Local {}
+
+    #[init]
+    fn init(mut ctx: init::Context) -> (Shared, Local) {
+        rtt_init_print!();
+        rprintln!("Start!!!");
+        // Set up CPU peripherals
+        let mut cp = ctx.core;
+        // Set up microcontroller peripherals
+        let mut dp = pac::Peripherals::take().unwrap();
+
+        let mut clock_cfg = Clocks::default(); //400MHz
+
+        // ---------------clock---------------
+        clock_cfg.pll1 = PllCfg {
+            enabled: true,
+            pllp_en: true,
+            pllq_en: true,
+            pllr_en: true,
+            divm: 32,
+            divn: 200,
+            divp: 2,
+            divq: 4,
+            divr: 2,
+        };
+
+        clock_cfg.setup().unwrap();
+
+        // Configure pins for Spi
+        let _sck = Pin::new(Port::A, 12, PinMode::Alt(5));
+        let _miso = Pin::new(Port::B, 14, PinMode::Alt(5));
+        let _mosi = Pin::new(Port::B, 15, PinMode::Alt(5));
+        let mut nss = Pin::new(Port::A, 11, PinMode::Output);
+        nss.set_high();
+
+        let mut led = Pin::new(Port::D, 0, PinMode::Output);
+        led.set_high();
+
+        // `SpiConfig::default` is mode 0, full duplex, with software CS.
+        let spi2_cfg = SpiConfig {
+            mode: SpiMode::mode1(),
+            ..Default::default()
+        };
+
+        let mut spi2 = Spi::new(
+            dp.SPI2,
+            spi2_cfg,
+            BaudRate::Div64,
+        );
+
+        let mut dma = Dma::new(dp.DMA1);
+
+        dma::mux(DmaPeriph::Dma1, DmaChannel::C1, DmaInput::Spi2Tx);
+        dma::mux(DmaPeriph::Dma1, DmaChannel::C2, DmaInput::Spi2Rx);
+
+        let mut spi_buf: [u8; 8] = [0x00, 0x00, 0xAA, 0xAA, 0x00, 0x00, 0xD0, 0xAB];
+        nss.set_low();
         // delay.delay_us(1_00);
-        spi.transfer(&mut spi_buf).ok();
+        spi2.transfer(&mut spi_buf).ok();
         let values = spi_buf;
         for (i, &value) in values.iter().enumerate() {
-            rprintln!("Received data 2 {}: {}", i, value);
+            rprintln!("Received data 1 {}: {:#010x}", i, value);
+        }
+        nss.set_high();
+
+        (
+            Shared {
+                dma,
+                spi2,
+                nss,
+            },
+            Local {},
+        )
+    }
+
+    #[idle(shared = [spi2, nss])]
+    fn idle(mut ctx: idle::Context) -> ! {
+        (ctx.shared.nss, ctx.shared.spi2).lock(|nss, spi2| {
+            nss.set_low();
+    
+            unsafe {
+                spi2.transfer_dma(
+                    &SPI_WRITE_BUF,
+                    &mut SPI_READ_BUF,
+                    DmaChannel::C1,
+                    DmaChannel::C2,
+                    dma::ChannelCfg {
+                        priority: dma::Priority::Medium,
+                        circular: dma::Circular::Disabled,
+                        periph_incr: dma::IncrMode::Disabled,
+                        mem_incr: dma::IncrMode::Enabled,
+                    },
+                    dma::ChannelCfg {
+                        priority: dma::Priority::Medium,
+                        circular: dma::Circular::Disabled,
+                        periph_incr: dma::IncrMode::Disabled,
+                        mem_incr: dma::IncrMode::Enabled,
+                    },
+                    DmaPeriph::Dma1,
+                );
+            }
+            rprintln!("transferd!!!");
+        });
+
+        loop {
+        }
+    }
+
+    #[task(binds = DMA1_STR1, shared = [dma], priority = 2)]
+    fn write_interrupt(mut ctx: write_interrupt::Context) {
+        (ctx.shared.dma).lock(|dma| {
+            let is = dma.transfer_is_complete(DmaChannel::C1);
+            rprintln!("transfer is complete? : {}", is);
+
+            dma::clear_interrupt(DmaPeriph::Dma1, DmaChannel::C1, DmaInterrupt::TransferComplete);
+        });
+        rprintln!("transmit complete");
+    }
+
+    #[task(binds = DMA1_STR2, shared = [spi2, nss], priority = 2)]
+    fn read_interrupt(mut ctx: read_interrupt::Context) {
+        dma::clear_interrupt(DmaPeriph::Dma1, DmaChannel::C2, DmaInterrupt::TransferComplete);
+
+        (ctx.shared.spi2).lock(|spi| {
+            spi.stop_dma(DmaChannel::C2, Some(DmaChannel::C1), DmaPeriph::Dma1);
+        });
+
+        ctx.shared.nss.lock(|nss| {
+            nss.set_high();
+        });
+
+        let values = unsafe { SPI_READ_BUF };
+        for (i, &value) in values.iter().enumerate() {
+            rprintln!("Received data 2 {}: {:#010x}", i, value);
         }
         let angle_lsb = ((values[1] & 0x3F) as u16) << 8 | (values[0] as u16);
         rprintln!("angle {}", angle_lsb);
@@ -132,6 +180,5 @@ fn main() -> ! {
         rprintln!("vgain {}", vgain_lsb);
         let rollcnt_lsb = (values[6] as u16) & 0x3F;
         rprintln!("rollcnt {}", rollcnt_lsb);
-        cs.set_high();
     }
 }
